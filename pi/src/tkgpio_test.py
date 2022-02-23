d9c5eaa00f7bc9390489dbdc5aeb74db7468cc58
Yousef Yassin <yousefyassin@cmail.carleton.ca>
2022-02-23 16:39:39 -0500

    Installed tkgpio and created simple test.

--- pi/src/tkgpio_test.py
+++ pi/src/tkgpio_test.py
@@ -17,27 +17,25 @@ configuration = {
 }
 
 circuit = TkCircuit(configuration)
+
+
 @circuit.run
-def main ():
-    
+def main():
     # now just write the code you would use in a real Raspberry Pi
-    
+
     from gpiozero import LED, Button
     from time import sleep
-    
-    
+
     led1 = LED(21)
     led1.blink()
-    
-    
+
     def button_pressed():
         print("button pressed!")
         led2.toggle()
-    
+
     led2 = LED(22)
     button = Button(11)
     button.when_pressed = button_pressed
-    
-    
+
     while True:
         sleep(0.1)
