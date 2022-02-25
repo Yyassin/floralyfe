6e1c5f82d37709dc970e428afaf06fd6cb9ef9bb
Yousef Yassin <yousefyassin@cmail.carleton.ca>
2022-02-25 01:24:30 -0500

    Configured firestore cloud and local testing.

---
+++ server/config.ts
@@ -0,0 +1,37 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+// Version 9 imports: https://stackoverflow.com/questions/69139443/property-auth-does-not-exist-on-type-typeof-import-firebase-auth/69489577#69489577
+
+const testing = (process.env.TEST !== undefined);
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+}
+
+const firebaseTestConfig = {
+    apiKey: process.env.FIREBASE_TEST_API_KEY,
+    authDomain: process.env.FIREBASE_TEST_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_TEST_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_TEST_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_TEST_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_TEST_APP_ID,
+}
+
+firebase.initializeApp(testing ? firebaseTestConfig : firebaseConfig);
+// const auth = firebase.auth()
+const db = firebase.firestore();
+if (testing) {
+    console.log("connecting emulator")
+    db.useEmulator("localhost", 8080);
+}
+
+const User = db.collection("Users");
+
+export { User }
--- server/src/server.ts
+++ server/src/server.ts
@@ -7,17 +7,40 @@
 require("dotenv").config();
 
 import express from "express";
+import bodyParser from "body-parser";
 import http from "http";
+import cors from "cors";
 import { setupWebSocket } from "./setupWebSocket";
+import { User } from "../config";
 
 const app = express();
-const port = process.env.PORT || 4000; // Default port to listen
 
+app.use(express.json())                                 // parse application/json
+app.use(bodyParser.urlencoded({ extended: false }))     // parse application/x-www-form-urlencoded
+app.use(cors());
+
+const port = process.env.PORT || 4001; // Default port to listen
 const server = http.createServer(app);
 
 // Define a route handler for the default home page
 app.get("/", (req, res) => {
     res.status(200).send("Hello world!");
+});
+
+app.get("/users", async (req, res) => {
+    const snapshot = await User.get();
+    const list = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    res.status(200).send(list);
+})
+
+app.post("/create", async (req, res) => {
+    const data = req.body;
+    // console.log(data)
+    await User.add(data);
+
+    res.status(200).send({
+        msg: "User added.",
+    });
 });
 
 // Start the Express server
@@ -29,4 +52,4 @@ server.listen(port, () => {
 // Setup and start associated WebSocket on distinct server ws://
 setupWebSocket(server);
 
-export { server };
+export { server, port };
