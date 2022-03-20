5e9f168d196f1c3269e8b2dfb0c522672e2ff384
Yousef Yassin <yousefyassin@cmail.carleton.ca>
2022-03-20 14:01:25 -0400

    Added machine to IS_RPI to avoid with linux workflow machine.

--- pi/src/util/util.py
+++ pi/src/util/util.py
@@ -1,4 +1,4 @@
 import platform
 
 IS_WINDOWS = platform.uname().system == "Windows"
-IS_RPI = not IS_WINDOWS  # not great but works for our purposes
+IS_RPI = not IS_WINDOWS and platform.machine() == "armv7l"  # not great but works for our purposes
